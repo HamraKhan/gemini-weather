7e93fecd12ecb3163cc28ffe2232a9d0969af828
Hamra <hamra.khan.b@gmail.com>
2022-09-13 23:32:02 +0100

    Set up node Express with react - configured vs code to run app locally using one terminal - added necessary testing libraries/packages

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is running with Node Express API! Woohooo!!
         </p>
         <a
           className="App-link"
