54ad2adb063420b5b9963ec68729d4eeac1c122d
Hamra Khan <78437448+HamraKhan@users.noreply.github.com>
2022-09-25 19:37:47 +0100

    GEM-18 (#3)
    - Set up styles folder structure to include files for colors, fonts, spacing and index(for exporting styles)
    - created config file using module.exports to add assets
    - loaded fonts in App.js using class component AppLoading
    - Removed png files not being used

---
+++ client/src/styles/colors.js
@@ -0,0 +1,14 @@
+//colors pallette
+export const H1 = '#2F2F2F';
+export const SEARCH_BAR_BKGD = '#E2DFDF';
+export const SELECTED_ICON = '#00C2FF';
+export const NEWSFEED_PAGE_BKGD = '#F2F2F2';
+export const SELECTED_FILTER_BTN = '#827F7F';
+export const DROPDOWN_LINE_BREAK = '#CFCFCF';
+export const LOCATIN_DROPDOWN_LIST = '#696767';
+export const OFF_WHITE = '#FBFBFB';
+export const OFF_BLACK = '#515151';
+export const RED = '#FF3D00';
+export const TRANSPARENT_WHITE = '#F4F4F4BF';
+export const WHITE = '#FFFFFF';
+export const BLACK = '#000000';
