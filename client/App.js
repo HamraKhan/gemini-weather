05bd97529e9302c0732f140cb06252bb6f9823c4
Hamra <hamra.khan.b@gmail.com>
2022-10-02 00:13:15 +0100

    GEM-7: added styling to render dashboard page in web view

--- client/App.js
+++ client/App.js
@@ -23,7 +23,9 @@ export default function App() {
   }
 
   return (
-    <View>
+    <View style={{
+          height: '-webkit-fill-available'
+        }}>
       <Dashboard />
       <StatusBar />
     </View>
