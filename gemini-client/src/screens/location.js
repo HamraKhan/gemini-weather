55a5b5b15041278d5117c214348d82f9b9231579
Hamra Khan <78437448+HamraKhan@users.noreply.github.com>
2022-10-12 16:54:24 +0100

    Gem 8 navbar component (#7)
    * deleted client folder and its contents
    
    * GEM-19
    - Updated github repo to switch from react native to react(web view) using 'concurrently' dependency
    
    * GEM-19
    - added images and favicon to assets folder
    - created variables for colors and imported it in index.css to be used (changed styling in App.css)
    - added <p> element to App.js
    - moved imported fonts to styles folder
    
    * GEM-8
    - installed jest and react test renderer for testing
    - installed ret icons and react router dom for routing
    -  Added routes for screens
    - Added BrowserRouter in index.js for react routing
    - Created Navbar component and added icons
    - Created dashboard, location and article screen pages
    - Added styling for App.css and navbar

---
+++ gemini-client/src/screens/location.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/navbar/navbar';
+
+const Location = () => {
+  return (
+    <div>
+      <p>This is the Location page</p>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Location;
